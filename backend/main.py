c3a420548d7cbca67608e30f2169957d8974af5f
Jeric Roque <jericjacobreece.roque@my.jru.edu>
2026-06-11 18:22:55 +0800

    Added Sponsors/Partners' links

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from routers.members import router as members_router
 from routers.events import router as events_router
 from routers.sponsors import router as sponsors_router
 
 app = FastAPI(title="AWS Learning Club API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(members_router)
 app.include_router(events_router)
